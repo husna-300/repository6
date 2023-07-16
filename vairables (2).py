eaaeb48aaa36eb14c61f88d6caad61b9a688d750
husna <h111126522@gmail.com>
2023-07-17 01:28:52 +0500

    my 6th repo

---
+++ vairables (2).py
@@ -0,0 +1,42 @@
+# addition
+def addition ():
+
+    price =200000
+    discount = 50000
+    print (price + discount)
+
+addition() 
+
+
+def divide ():
+   
+   book =3600011
+   student=87000 
+   print (book/student)
+
+divide()   
+
+
+def multiply():
+
+    money = 1776622
+    employees = 11770
+    print(money*employees)
+
+multiply()
+
+def subtraction ():
+    
+    dayOftheMonth =30
+    offDays = 17
+    print(dayOftheMonth-offDays)
+
+subtraction()   
+
+
+
+
+
+
+
+    
